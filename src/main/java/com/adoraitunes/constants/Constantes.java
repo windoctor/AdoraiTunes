d044cf5432b7bf4fb83d288965c3f1a54cba5b07
ASCARI ROMO PEDRAZA <ascariromopedraza@MacBook-Pro-de-ASCARI.local>
2016-06-26 19:30:19 -0500

    Constantes

---
+++ src/main/java/com/adoraitunes/constants/Constantes.java
@@ -0,0 +1,12 @@
+package com.adoraitunes.constants;
+
+/**
+ * Created by ascariromopedraza on 16/11/15.
+ */
+public class Constantes {
+
+    public static final String USUARIOS = "usuarios";
+    public static final String CATOLICA = "Catolica";
+    public static final String EVANGELICA = "Evangelica";
+
+}
